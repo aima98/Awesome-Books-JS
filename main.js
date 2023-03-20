f16054e1978eff3650a4fed3097da60d5b1b908a
Oumaima Nejjari <oumanj1998@gmail.com>
2023-03-20 13:56:43 +0000

    Add to main.js

---
+++ main.js
@@ -0,0 +1,59 @@
+let section = document.getElementById('section');
+let addBtn = document.getElementById('Add');
+let title = document.getElementById('title');
+let author = document.getElementById('author');
+
+// Creating empty array to store books
+let bookStorage = [];
+
+// CHecking if there are any books in Local Storage
+if(localStorage.getItem("books")) {
+  bookStorage = JSON.parse(localStorage.getItem("books"));
+}
+
+getItemsFromLocalStorage();
+
+// Adding a book
+addBtn.onclick = function() {
+  if(title.value != "" && author.value != "") {
+    addToBookStorage(title.value, author.value);
+    title.value = "";
+    author.value = ""; 
+  }
+}
+
+function addToBookStorage(titleName, authorName) {
+  const book = {
+    title: titleName,
+    author: authorName,
+  };
+
+  // Pushing books to bookStorage array
+  bookStorage.push(book);
+  // Adding books to page
+  addBookToPage(bookStorage);
+  // Adding books to local storage
+  addDataToLocalStorage(bookStorage);
+}
+
+function addBookToPage (bookStorage) {
+  // Creating empty section of books  
+  section.innerHTML = "";
+  // Adding books to the empty section of books  
+  bookStorage.forEach(book => {
+    section.innerHTML += `
+      <div id="book">
+        <label for="title">${book.title}</label> <br>
+        <label for="author">${book.author}</label>
+        <div>
+          <span>
+            <button onClick="deleteBook(this)" class="fas fa-trash-alt">Remove</button>
+          </span>
+        </div>
+        <hr>
+      </div>
+    `;
+    title.value = '';
+    author.value = '';
+  });
+}
